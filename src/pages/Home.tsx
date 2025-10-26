9a00a4da3b9afe6ff190e1c3d8d325dcd811f03e
chriskang <kangchenhe666@gmail.com>
2025-10-26 22:40:22 +0800

    feat: 登录页修改

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import HeaderNavigation from "@/features/home/HeaderNavigation";
+
+function Home() {
+  // TODO
+  return (
+    <>
+      <HeaderNavigation />
+    </>
+  );
+}
+
+export default Home;
