dcbe74486f0b52b3c6f5e519321ca7c117acb4b0
chriskang <kangchenhe666@gmail.com>
2025-10-26 22:40:22 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "@/features/login/Login";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
