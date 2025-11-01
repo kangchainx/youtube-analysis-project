60359a66fb8be362ea57073b3ea0f2bd2b2ac704
chriskang <kangchenhe666@gmail.com>
2025-11-01 13:40:14 +0800

    fix: 解决lint检查报错

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,32 @@
 import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
+export default [
+  { ignores: ["dist/**"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      parser: tseslint.parser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
   },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+];
